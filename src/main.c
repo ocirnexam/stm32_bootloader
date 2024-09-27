d0a6c2a33d33b3f1749f01dde17c6122ea3d5bfc
ocirnexam <kontakt.maxmueller@outlook.com>
2024-09-27 10:50:28 +0200

    Added CMSIS6, stm32f4 headers, startup and linker file, and configured cmake for arm-none-eabi-gcc

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include "stm32f4/stm32f4xx.h"
+
+#if !defined  (HSE_VALUE) 
+  #define HSE_VALUE    ((uint32_t)25000000) /*!< Default value of the External oscillator in Hz */
+#endif /* HSE_VALUE */
+
+#if !defined  (HSI_VALUE)
+  #define HSI_VALUE    ((uint32_t)16000000) /*!< Value of the Internal oscillator in Hz*/
+#endif /* HSI_VALUE */
+
+void SystemInit(void)
+{
+  /* FPU settings ------------------------------------------------------------*/
+  #if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
+    SCB->CPACR |= ((3UL << 10*2)|(3UL << 11*2));  /* set CP10 and CP11 Full Access */
+  #endif
+}
+
+int main(void)
+{
+    SystemInit();
+    return 0;
+}
